1e116549d5261b9b235b6aa92026089444936354
Lakshitha Fernando <lakshithaf@innosoftwareworld.com>
2019-03-18 12:25:55 +0530

    commit changes

--- api/routes/root-routes.js
+++ api/routes/root-routes.js
@@ -2,7 +2,7 @@
 
 const express = require('express');
 const router = express.Router();
-const asyncHandler = require('express-async-handler')
+const asyncHandler = require('express-async-handler');
 
 
 router.get('/', asyncHandler(async (req, res) => {
